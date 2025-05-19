3f8760a0f50521b9c3f91f7aa66738dd4fb08284
zinkirov174@gmail.com <zinkirov174@gmail.com>
2025-05-19 18:57:31 +0600

    first try

--- main.c
+++ main.c
@@ -1,2 +1,26 @@
-//Write your code in this file
+// Write your code in this file
+ #include <stdio.h>
+#include <stdlib.h>
 
+#define ROLL() ((rand() % 6) + 1)
+
+int main() {
+int counts[6] = {0}, sum = 0;
+
+srand(1);
+
+for (int i = 0; i < 100; i++) {
+int roll = ROLL();
+counts[roll - 1]++;
+sum += roll;
+}
+
+for (int i = 0; i < 6; i++) {
+printf("%d\n", counts[i]);
+}
+
+printf("%d\n", sum);
+printf("%.1f\n", (float)sum / 100);
+
+return 0;
+}
